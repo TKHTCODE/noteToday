c95db21dc65081e89dc744b8d10bc5a4c34ae837
TKHTS <sakuranohanabira1269@gmail.com>
2022-03-29 18:32:23 -0700

    Add settings to connect database

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,21 @@
 let express = require('express');
 let router = express.Router();
+let mongojs = require('mongojs');
+let MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+let uri = 'mongodb+srv://'+process.env.username+':'+process.env.password +'@cluster0.glfar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-router.get('/todos', function(req, res, next){
-    res.send('TODOS API');
+router.get('/todos', function(req, res){
+    MongoClient.connect(uri, {useNewUrlParser: true}, function(err, client){
+        var collection = client.db('todos').collection('todos');
+        collection.find().toArray(function(error, documents){
+            if(err){
+                res.send(err);
+            }else{
+                res.send(documents);
+            }
+        })
+    });
 });
 
 module.exports = router;
